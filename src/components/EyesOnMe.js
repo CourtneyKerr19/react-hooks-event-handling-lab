78cc9040cbd1557466c58b09222439c8569a3f2c
Courtney Kerr <courtneykerr219@gmail.com>
2024-04-10 22:11:23 -0500

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    // Function to call when the button is focused
+    function handleFocus() {
+        console.log('Good!');
+    }
+
+    // Function to call when the button loses focus
+    function handleBlur() {
+        console.log('Hey! Eyes on me!');
+    }
+
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
